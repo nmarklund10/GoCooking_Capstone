e7e86e0a2206d6e5848f1e4a37d21a6d90c06f9b
Neil Marklund <nmarklund10@hotmail.com>
2018-03-21 13:59:58 -0500

    made image lookup by keyword

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -6,6 +6,64 @@ function getStaticResource(url) {
     return '/static/' + url
 }
 
+function getIngredientImage(ingredient) {
+    path = 'images/recipes/ingredients/';
+    ingredient = ingredient.toLowerCase();
+    if(ingredient.includes("english muffin")) {
+        return getStaticResource(path + 'english_muffin.jpg');
+    }
+    else if(ingredient.includes("egg")) {
+        return getStaticResource(path + 'egg.jpg');
+    }
+    else if(ingredient.includes("green onion")) {
+        return getStaticResource(path + 'green_onions.jpg');
+    }
+    else if(ingredient.includes("cheddar cheese")) {
+        return getStaticResource(path + 'cheddar_cheese.jpg');
+    }
+    else if(ingredient.includes("green onion")) {
+        return getStaticResource(path + 'green_onions.jpg');
+    }
+    else if(ingredient.includes("vegetable oil")) {
+        return getStaticResource(path + 'vegetable_oil.jpg');
+    }
+    else if(ingredient.includes("breakfast sausage")) {
+        return getStaticResource(path + 'breakfast_sausage.jpg');
+    }
+    else {
+        return getStaticResource('images/members/cameron.png');
+    }
+}
+
+function getEquipmentImage(equipment) {
+    path = 'images/recipes/equipment/';
+    equipment = equipment.toLowerCase();
+    if(equipment.includes("bowl")) {
+        return getStaticResource(path + 'bowl.jpg');
+    }
+    else if(equipment.includes("fork")) {
+        return getStaticResource(path + 'fork.jpg');
+    }
+    else if(equipment.includes("paper towel")) {
+        return getStaticResource(path + 'paper_towels.jpg');
+    }
+    else if(equipment.includes("skillet")) {
+        return getStaticResource(path + 'skillet.jpg');
+    }
+    else if(equipment.includes("spatula")) {
+        return getStaticResource(path + 'spatula.jpg');
+    }
+    else if(equipment.includes("toaster oven")) {
+        return getStaticResource(path + 'toaster_oven.jpg');
+    }
+    else if(equipment.includes("whisk")) {
+        return getStaticResource(path + 'whisk.jpg');
+    }
+    else {
+        return "";
+    }
+}
+
 function catchGenericError(error) {
     console.log(error);
     window.alert("An error occured on the server.");
