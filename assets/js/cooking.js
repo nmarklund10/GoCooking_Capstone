f37b3e112a038e39b6d9206d1c82a0120c41a035
Neil Marklund <nmarklund10@hotmail.com>
2018-03-30 00:50:18 -0500

    finished evaluation page (#34)
    * clone from master
    
    * setup Main screen and buttons go to different screens
    
    * update
    
    * removed unnecessary string
    
    * Set up search recipe screen with cards that will display recipe information
    
    * Implemented search bar on recipe page
    
    * Load in real recipes on search recipe page.
    
    * Made loading in recipes from server asynchronous and fixed search functionality on Search Recipe page.
    
    * Made recipe popup view
    
    * Changing from Android to Django App
    
    * Set up base template for project
    
    * made template with material design lite
    
    * "made login page"
    
    * fixed some layout things
    
    * implemented login
    
    * removed unnecessary dependencies
    
    * updated about page
    
    * Created Skills page for eggs, added initial data, restructured image directory
    
    * made alert box into mdl dialog
    
    * update git ignore
    
    * update
    
    * Add recipe overview
    
    * update
    
    * Finished get more info screen
    
    * fixed displaying name
    
    * pull
    
    * fixed errors on dashboard
    
    * Neil (#20)
    
    * Finished get more info screen
    
    * fixed displaying name
    
    * fixed errors on dashboard
    
    * made image lookup by keyword
    
    * added default for equipment
    
    * added rest of images
    
    * start main cooking page
    
    * implemented basic cooking page
    
    * wrote skeleton code for setting gif
    
    * added warning dialog for completing recipe
    
    * update dashboard
    
    * update
    
    * trying to merge
    
    * merge
    
    * delete header
    
    * finished implementing dashboard locking recipes user hasn't unlocked yet
    
    * added skills to recipe overview
    
    * update exit button
    
    * update cooking page
    
    * add button border
    
    * exit alert
    
    * finished timer
    
    * changed skills look and cooking page
    
    * add assessment dialog
    
    * Added timer sound
    
    * finished evaluation page
    
    * removed commented lines

--- assets/js/cooking.js
+++ assets/js/cooking.js
@@ -1,3 +1,24 @@
+function getPicName(name) {
+    if (name == "Muffin Morning Makers") {
+        return "egg1";
+    }
+    else if (name == "Ham and Cheese Omelet") {
+        return "egg2";
+    }
+    else if (name == "Vegetable Stovetop Frittata") {
+        return "egg3";
+    }
+    else if (name == "Bacon Chicken Sandwiches") {
+        return "chicken1";
+    }
+    else if (name == "Chicken Parmesan") {
+        return "chicken2";
+    }
+    else if (name == "Crispy Fried Chicken") {
+        return "chicken3";
+    }
+}
+
 function setup() {
     sendGetRequestForJSON("/getARecipe/", {'recipe': recipe}, 
     function(response) {
@@ -6,6 +27,7 @@ function setup() {
             window.recipe.instructions = JSON.parse(recipe.instructions);
             window.recipe.ingredients = JSON.parse(recipe.ingredients);
             window.recipe.equipment = JSON.parse(recipe.equipment);
+            window.recipe.skills = JSON.parse(recipe.skills);
             document.getElementById('recipeTitle').innerText = window.recipe.name;
             window.stepNumber = 0;
             updateCurrentStep();
@@ -16,6 +38,7 @@ function setup() {
     document.getElementById('closeDialogButton').onclick = function() { document.getElementById('alertDialog').close(); };
     document.getElementById('cancelDialogButton').onclick = function() { document.getElementById('exitDialog').close(); };
     window.timerSound = new Audio(getStaticResource("audio/timerEnd.mp3"));
+    window.selectedImage = null;
 }
 
 function nextStep() {
@@ -47,9 +70,11 @@ function updateCurrentStep() {
     }
 }
 
+
 function exitWindow() {
     document.getElementById('exitDialog').showModal();
 }
+
 
 function setTimer(step) {
     step = step.toLowerCase();
@@ -60,7 +85,6 @@ function setTimer(step) {
     if (index > -1) {
         document.getElementById('timerButton').style.visibility = "visible";
         var length = step[index - 1] * 60;
-        length = 5;
         document.getElementById('timerButton').addEventListener('click', function(){
             document.getElementById('timerButton').style.color = "rgb(33,150,243)"            
             document.getElementById('timerButton').innerText = length + " sec";
@@ -100,8 +124,155 @@ function closeTimer() {
     document.getElementById('timerDialog').close();
 }
 
+function selectImage(i) {
+    var allImages = document.getElementsByClassName('assessPics');
+    for (var j = 0; j < allImages.length; j++) {
+        allImages[j].style.borderColor = "white";
+    }
+    allImages[i].style.borderColor = "#2196F3";
+    window.selectedImage = allImages[i];
+}
+
+function getRadioButtonValue(className) {
+    radioButtons = document.getElementsByClassName(className);
+    checkedValue = -1;
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            checkedValue = parseInt(radioButtons[i].value);
+            break;
+        }
+    }
+    return checkedValue;
+}
+
+function getImageValue(src) {
+    src = src.substr(src.length - 5);
+    if (src == "0.jpg")
+        return 1;
+    if (src == "1.jpg")
+        return 3;
+    if (src == "2.jpg")
+        return 5;
+}
+
+function submitAssessment() {
+    var score = 0;
+    if (window.selectedImage == null) {
+        document.getElementById('assessError').showModal();
+        return;
+    }
+    score += getImageValue(window.selectedImage.src);
+    var curValue = getRadioButtonValue('satisfy');
+    if (curValue == -1) {
+        document.getElementById('assessError').showModal();
+        return;
+    }
+    score += curValue;
+    for (var i = 0; i < window.recipe.skills.length; i++) {
+        curValue = getRadioButtonValue('skill' + i);        
+        if (curValue == -1) {
+            document.getElementById('assessError').showModal();   
+            return;
+        }
+        score += curValue;
+    }
+    score /= (window.recipe.skills.length + 2).toFixed(2);
+    if (score >= 3.5) {
+        sendPostRequest("/passed/", {"recipe": window.recipe.name}, 
+        function(response) {
+            if (response.success) {
+                goToUrl("/dashboard");
+            }
+            else {
+                alert(response.reason);
+            }
+        });
+    }
+    else {
+        document.getElementById('failScreen').showModal();
+    }
+}
+
 function finishRecipe() {
-    goToUrl('/dashboard');
+    var images = document.getElementsByClassName('assessPics');
+    var name = getPicName(window.recipe.name);
+    var sources = [name + 0, name + 1, name + 2];
+    var selectedSource = sources[getRandomInt(0, 2)];
+    images[0].src = getStaticResource("images/recipes/evaluation/" + selectedSource + ".jpg");
+    remove(sources, selectedSource);
+    selectedSource = sources[getRandomInt(0, 1)];
+    images[1].src = getStaticResource("images/recipes/evaluation/" + selectedSource + ".jpg");
+    remove(sources, selectedSource);
+    images[2].src = getStaticResource("images/recipes/evaluation/" + sources[0] + ".jpg");
+    var content = document.getElementById('assessContent');
+    var dishGrid = document.createElement('div');
+    dishGrid.className = "mdl-grid assess";
+    content.appendChild(dishGrid);
+    var dishCell = document.createElement('div');
+    dishCell.className = "mdl-cell assess";
+    dishGrid.appendChild(dishCell);
+    var spacer = document.createElement('div');
+    spacer.className = "assessSpacer";
+    dishCell.appendChild(spacer);
+    for (var i = 0; i < 5; i++) {
+        var label = document.createElement('label');
+        label.className = "mdl-radio mdl-js-radio";
+        label.htmlFor = "option" + parseInt(i);
+        var input = document.createElement('input');
+        input.type = "radio";
+        input.id = label.htmlFor;
+        input.name = "satisfy";
+        input.value = i+1;
+        input.className = "mdl-radio__button " + input.name;
+        var span = document.createElement('span');
+        span.className = "mdl-radio__label";
+        span.innerHTML = i+1;
+        if (i == 0)
+            span.innerHTML += "<br>Not at All";
+        else if (i == 4)
+            span.innerHTML += "<br>Loved It";            
+        dishCell.appendChild(label);
+        label.appendChild(input);
+        label.appendChild(span);
+    }
+    for (var i = 0; i < window.recipe.skills.length; i++) {
+        var header = document.createElement('div');
+        header.innerText = "How confident do you now feel with " + window.recipe.skills[i] + "?";
+        header.className = "assessmentQuestion";
+        content.appendChild(header);
+        var grid = document.createElement('div');
+        grid.className = "mdl-grid assess";
+        content.appendChild(grid);
+        var cell = document.createElement('div');
+        cell.className = "mdl-cell assess";
+        grid.appendChild(cell);
+        var spacer = document.createElement('div');
+        spacer.className = "assessSpacer";
+        cell.appendChild(spacer);
+        for (var j = 0; j < 5; j++) {
+            var label = document.createElement('label');
+            label.className = "mdl-radio mdl-js-radio";
+            label.htmlFor = "option" + parseInt(i) + parseInt(j);
+            var input = document.createElement('input');
+            input.type = "radio";
+            input.id = label.htmlFor;
+            input.name = "skill" + i;
+            input.value = j+1;
+            input.className = "mdl-radio__button " + input.name;
+            var span = document.createElement('span');
+            span.className = "mdl-radio__label";
+            span.innerHTML = j+1;
+            if (j == 0)
+                span.innerHTML += "<br>Not at All";
+            else if (j == 4)
+                span.innerHTML += "<br>Very Confident";            
+            cell.appendChild(label);
+            label.appendChild(input);
+            label.appendChild(span);
+        }
+    }
+    componentHandler.upgradeElements(content);
+    document.getElementById('assessmentDialog').showModal();
 }
 
 function getGif(instruction) {
